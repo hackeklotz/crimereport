c131ce0601cedab3a56326ad7d9cd19ab6db6d49
hackeklotz <hackeklotz@mailbox.org>
2018-11-04 21:20:22 +0100

    Add action for previous report

--- frontend/src/components/crimeselector/crimeSelector.tsx
+++ frontend/src/components/crimeselector/crimeSelector.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { IStoreState } from 'src/components/types';
-import { INextReport, nextReport } from './crimeSelectorRedux';
+import { ISwitchReport, nextReport, previousReport } from './crimeSelectorRedux';
 
 interface IProps {
   day: string;
@@ -30,10 +30,10 @@ function mapStateToProps({ day }: IStoreState) {
   }
 }
 
-function mapDispatchToProps(dispatch: Dispatch<INextReport>) {
+function mapDispatchToProps(dispatch: Dispatch<ISwitchReport>) {
   return {
       onNext: () => dispatch(nextReport()),
-      onPrevious: () => dispatch(nextReport()),
+      onPrevious: () => dispatch(previousReport()),
   }
 }
 
--- frontend/src/components/crimeselector/crimeSelectorRedux.tsx
+++ frontend/src/components/crimeselector/crimeSelectorRedux.tsx
@@ -2,26 +2,42 @@ import { IStoreState } from 'src/components/types';
 
 // actions
 export const NEXT_REPORT = 'NEXT_REPORT';
+export const PREVIOUS_REPORT = 'PREVIOUS_REPORT';
 
 // action creators
-export interface INextReport {
+export interface ISwitchReport {
     type: string;
 }
 
-export function nextReport(): INextReport {
+export function nextReport(): ISwitchReport {
     return {
         type: NEXT_REPORT
     }
 }
 
+export function previousReport(): ISwitchReport {
+    return {
+        type: PREVIOUS_REPORT
+    }
+}
+
 // reducers
-export function switchReport(state: IStoreState, action: INextReport): IStoreState {
+export function switchReport(state: IStoreState, action: ISwitchReport): IStoreState {
     switch (action.type) {
-        case NEXT_REPORT:            
-            let num = parseInt(state.day.slice(5), 10);
-            num++;
-            const day2 = 'today'+num;
-            return { ...state, day: day2 };
+        case NEXT_REPORT:
+            {
+                let num = parseInt(state.day.slice(5), 10);
+                num++;
+                const day2 = 'today' + num;
+                return { ...state, day: day2 };
+            }
+        case PREVIOUS_REPORT:
+            {
+                let num = parseInt(state.day.slice(5), 10);
+                num--;
+                const day2 = 'today' + num;
+                return { ...state, day: day2 };
+            }
     }
     return state;
 }
