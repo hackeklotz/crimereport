558f23c43c2492226169856a17ffe880b93beaf9
hackeklotz <hackeklotz@mailbox.org>
2019-03-13 23:14:40 +0100

    Bugfix: correct order of latitude and longitude

--- rest-service/src/main/java/crimereport/crimes/CrimeBuilder.java
+++ rest-service/src/main/java/crimereport/crimes/CrimeBuilder.java
@@ -37,8 +37,8 @@ public class CrimeBuilder {
 
 		Geometry point = new Geometry();
 		double[] coordinates = new double[2];
-		coordinates[0] = longitude;
-		coordinates[1] = latitude;
+		coordinates[0] = latitude;
+		coordinates[1] = longitude;
 		point.setCoordinates(coordinates);
 		crime.setGeometry(point);
 	}
