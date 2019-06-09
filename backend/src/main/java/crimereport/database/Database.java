aa06a347c79ab1f6e7324ff30e1a62864e14a08f
hackeklotz <hackeklotz@mailbox.org>
2019-06-10 00:27:19 +0200

    Add query parameter to filter the region

--- backend/src/main/java/crimereport/database/Database.java
+++ backend/src/main/java/crimereport/database/Database.java
@@ -9,10 +9,7 @@ import java.util.TreeSet;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 
 import crimereport.crimes.Crime;
@@ -21,85 +18,89 @@ import crimereport.main.CrimeEndpoint;
 
 public class Database {
 
-	private String url;
+    private String url;
 
-	private static Logger LOGGER = LogManager.getLogger(CrimeEndpoint.class);
+    private static Logger LOGGER = LogManager.getLogger(CrimeEndpoint.class);
 
-	public Database(String url) {
-		LOGGER.info("Init database with url " + url);
-		this.url = url;
-		try {
-			Class.forName("org.sqlite.JDBC");
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public Database(String url) {
+        LOGGER.info("Init database with url " + url);
+        this.url = url;
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public List<Crime> getCrimes() {
-		LOGGER.info("Retrieve crimes");
-		try (Connection conn = DriverManager.getConnection(url)) {
-			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
-			Result<Record> result = create.select().from("crime").fetch();
+    public List<Crime> getCrimes() {
+        LOGGER.info("Retrieve crimes");
+        try (Connection conn = DriverManager.getConnection(url)) {
+            DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
+            Result<Record> result = create.select().from("crime").fetch();
 
-			List<Crime> crimes = new ArrayList<>();
-			for (Record record : result) {
-				Crime crime = buildCrime(record);
-				crimes.add(crime);
-			}
-			return crimes;
-		} catch (SQLException e) {
-			LOGGER.error("Couldn't retrieve crimes", e);
-			return new ArrayList<>();
-		}
-	}
+            List<Crime> crimes = new ArrayList<>();
+            for (Record record : result) {
+                Crime crime = buildCrime(record);
+                crimes.add(crime);
+            }
+            return crimes;
+        } catch (SQLException e) {
+            LOGGER.error("Couldn't retrieve crimes", e);
+            return new ArrayList<>();
+        }
+    }
 
-	public List<Crime> getReport(String id) {
-		LOGGER.info("Retrieve report with ID " + id);
-		try (Connection conn = DriverManager.getConnection(url)) {
-			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
-			Result<Record> result = create.select().from("crime")
-					.where(String.format("reportId = \"%s\"", id)).fetch();
+    public List<Crime> getReport(String id, String region) {
+        LOGGER.info("Retrieve report with ID " + id);
+        try (Connection conn = DriverManager.getConnection(url)) {
+            DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
+            SelectConditionStep<Record> query = create.select().from("crime")
+                    .where(String.format("reportId = \"%s\"", id));
+            if (region != null) {
+                query = query.and(String.format("region = \"%s\"", region));
+            }
+            Result<Record> result = query.fetch();
 
-			List<Crime> crimes = new ArrayList<>();
-			for (Record record : result) {
-				Crime crime = buildCrime(record);
-				crimes.add(crime);
-      }
-      return crimes;
-		} catch (SQLException e) {
-			LOGGER.error("Couldn't retrieve crimes", e);
-			return new ArrayList<>();
-		}
-	}
+            List<Crime> crimes = new ArrayList<>();
+            for (Record record : result) {
+                Crime crime = buildCrime(record);
+                crimes.add(crime);
+            }
+            return crimes;
+        } catch (SQLException e) {
+            LOGGER.error("Couldn't retrieve crimes", e);
+            return new ArrayList<>();
+        }
+    }
 
-	private Crime buildCrime(Record record) {
-		String title = (String) record.get("title");
-		String message = (String) record.get("message");
+    private Crime buildCrime(Record record) {
+        String title = (String) record.get("title");
+        String message = (String) record.get("message");
 
-		CrimeBuilder crimeBuilder = new CrimeBuilder(title, message);
+        CrimeBuilder crimeBuilder = new CrimeBuilder(title, message);
 
-		Object longitude = record.get("longitude");
-		Object latitude = record.get("latitude");
-		if (latitude != null && longitude != null) {
-			crimeBuilder.setCoordinate((double) latitude, (double) longitude);
-		}
-		return crimeBuilder.build();
-	}
+        Object longitude = record.get("longitude");
+        Object latitude = record.get("latitude");
+        if (latitude != null && longitude != null) {
+            crimeBuilder.setCoordinate((double) latitude, (double) longitude);
+        }
+        return crimeBuilder.build();
+    }
 
-	public TreeSet<String> getAllReportIds() {
-		LOGGER.info("Retrieve all report IDs");
-		try (Connection conn = DriverManager.getConnection(url)) {
-			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
-			Result<Record> result = create.select().from("crime").fetch();
+    public TreeSet<String> getAllReportIds() {
+        LOGGER.info("Retrieve all report IDs");
+        try (Connection conn = DriverManager.getConnection(url)) {
+            DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
+            Result<Record> result = create.select().from("crime").fetch();
 
-			TreeSet<String> reportIds = new TreeSet<>();
-			for (Record record : result) {
-				reportIds.add(record.get("reportId", String.class));
-			}
-			return reportIds;
-		} catch (SQLException e) {
-			LOGGER.error("Couldn't retrieve crimes", e);
-			return new TreeSet<>();
-		}
-	}
+            TreeSet<String> reportIds = new TreeSet<>();
+            for (Record record : result) {
+                reportIds.add(record.get("reportId", String.class));
+            }
+            return reportIds;
+        } catch (SQLException e) {
+            LOGGER.error("Couldn't retrieve crimes", e);
+            return new TreeSet<>();
+        }
+    }
 }
--- backend/src/test/java/crimereport/main/ReportEndpointTest.java
+++ backend/src/test/java/crimereport/main/ReportEndpointTest.java
@@ -1,6 +1,8 @@
 package crimereport.main;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import crimereport.crimes.Crime;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,15 +11,47 @@ class ReportEndpointTest {
 
     @Test
     void getAllReportIds() {
+        ReportEndpoint reportEndpoint = createReportEndpoint();
+
+        List<String> allReportIds = reportEndpoint.getAllReportIds();
+
+        assertThat(allReportIds).containsExactly("MI_2019_123", "MI_2019_456", "MI_2019_789");
+    }
+
+    @Test
+    void getReport() {
+        ReportEndpoint reportEndpoint = createReportEndpoint();
+
+        List<Crime> crimes = reportEndpoint.getReport("MI_2019_123", null);
+
+        assertThat(crimes).hasSize(1);
+        Crime crime = crimes.get(0);
+        assertThat(crime.getTitle()).isEqualTo("Katze weg");
+    }
+
+    @Test
+    void getReport_FilterRegion_Match() {
+        ReportEndpoint reportEndpoint = createReportEndpoint();
+
+        List<Crime> crimes = reportEndpoint.getReport("MI_2019_123", "Landeshauptstadt Dresden");
+
+        assertThat(crimes).hasSize(1);
+    }
+
+    @Test
+    void getReport_FilterRegion_NoMatch() {
+        ReportEndpoint reportEndpoint = createReportEndpoint();
+
+        List<Crime> crimes = reportEndpoint.getReport("MI_2019_123", "Dräsd´n");
+
+        assertThat(crimes).isEmpty();
+    }
+
+    private ReportEndpoint createReportEndpoint() {
         String path = getClass().getClassLoader().getResource("test.sq3").getPath();
         String url = "jdbc:sqlite:" + path;
         DatabaseProperties databaseProperties = new DatabaseProperties();
         databaseProperties.setUrl(url);
-        ReportEndpoint reportEndpoint = new ReportEndpoint(databaseProperties);
-
-        List<String> allReportIds = reportEndpoint.getAllReportIds();
-
-        assertThat(allReportIds).containsExactly("MI_2019_123","MI_2019_456","MI_2019_789");
+        return new ReportEndpoint(databaseProperties);
     }
-
 }
